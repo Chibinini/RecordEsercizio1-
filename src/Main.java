ace16a57fe9d23b9b9b1ff9dc85e1b98a97dff57
Chibinini <pirainocostanza@gmail.com>
2024-11-10 17:06:54 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Punto p = new Punto(3, 4);
+
+        // Accesso alle coordinate usando i getter
+        System.out.println("Coordinata x: " + p.x());
+        System.out.println("Coordinata y: " + p.y());
+
+
+    }
+}
